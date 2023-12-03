87be28b44678eafd487e98d2d3fb50beb058ecc7
iveshenry18 <iveshenry18@gmail.com>
2023-12-02 16:55:59 -0800

    chore: add further tweaks to try to fix crash

--- sonicsearch-app/src-tauri/build.rs
+++ sonicsearch-app/src-tauri/build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    println!("HELLO???");
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lomp");
         println!("cargo:rustc-link-search=native=/usr/local/lib");
+        println!("cargo:rustc-link-search=native=/opt/homebrew/Cellar/libomp/17.0.6/lib");
+        println!("cargo:rustc-link-lib=dylib=omp");
         println!("cargo:rustc-link-lib=dylib=faiss_c");
     } else if cfg!(target_os = "linux") {
         println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lstdc++");
